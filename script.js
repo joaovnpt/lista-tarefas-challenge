bf4062f81cc92057b5671c3b90d1c89b0c995435
joaovnpt <joaovitorguiotti8@gmail.com>
2023-12-06 14:55:52 -0300

    falta css

--- script.js
+++ script.js
@@ -1,54 +1,33 @@
-function adicionar() { //Função para adicionar uma nova tarefa
-    let novoItem = document.createElement("li"); //novoItem = criar elemento de lista
-    let textoItem = document.getElementById("tarefa").value; //textoItem = valor do elemento com id "tarefa"
-    let length = textoItem.length;
-    if (length < 1) alert("Digite a tarefa para adiciona-la");
-    else {
-        let textoNode = document.createTextNode(textoItem); //textoNode = transforma o textoItem de VALOR para TEXTO
-        novoItem.appendChild(textoNode); //textoNode agora é um filho de novoItem
-        let lista = document.getElementById("lista"); //lista = elemento com id "lista"
-        lista.appendChild(novoItem); //novoItem é agora um filho de lista
-        criarBotaoConcluir(novoItem) //Função para criar o botão de concluir tarefa para cada novoItem criado
+let addButton = document.getElementById("adicionar-tarefa");
+let containertask = document.getElementById("containertask");
+let inputcamp = document.getElementById("camporesult");
+let completetask = document.getElementById("containercompletetask");
+
+function additem(){ //função de adcionar item parágrafo
+    let task = document.createElement("p"); //é criad o elemento que vai para uma let
+    let buttonFinish = document.createElement("button") // cria o botão de finalizar ao lado da tarefa
+    buttonFinish.textContent = "Finish" // texto que fica no botão
+    inputcamp.focus(); // ira fazer que o foco fique sempre no input
+    task.innerText = inputcamp.value; // adciona o texto do input dentro do elemento p que vai para a lista
+    containertask.appendChild(task); // adciona o elemento p no espaço de tarefas pendentes
+    task.appendChild(buttonFinish) // adciona o botão de finalizar ao lado do elemento p
+    inputcamp.value = "" // limpa o texto digitado pelo usuario no input
+    task.addEventListener("click", function(){ //função para remover da lista de tarefas pendentes e passar o elemento p para a lista de tarefas concluidas
+        containertask.removeChild(task); // remove o elemento p da lista de tarefas pendentes
+        completetask.appendChild(task); // adciona o elemento p na lista de tarefas concluidas
+        task.removeChild(task.childNodes[1]) // remove o botão de finalizar
+        inputcamp.focus();
+    })
+    document.getElementById("limpar2").addEventListener("click", function(){
+       document.getElementById("containercompletetask").removeChild(task); // limpa os elementos da lista de tarefas concluidas
+       inputcamp.focus();
+    })
+}
+addButton.addEventListener("click", additem) // adciona o item na lista de pendente
+inputcamp.addEventListener("keyup", function(e){ // função de adcionar o item na lista através do botão enter
+    if(e.keyCode == 13){ // só chama a função quando for a tecla 13 (enter)
+        additem()
     }
-}
 
-function transferirTarefa() {
-    let divFinalizadas = document.querySelector(".finalizadas") //divFinalizadas = div .finalizadas
-    let lista = document.getElementById("lista");
-    let itemParaRemover = lista.lastChild; //itemParaRemover = ultimo filho da lista
-    if (itemParaRemover) {
-        divFinalizadas.appendChild(lista) //Se houver um itemParaRemover, a tarefa é transferida para a div .finalizadas
-        let botao = document.getElementById("botao-concluir");
-        excluirBotaoConcluir(botao) //Exclui o botão junto
-        let novoItem = lista.lastChild; //novoItem = último filho da lista
-        transferirTarefa(novoItem); //Função já criada com parâmetro do item que quero excluir
-    }
-}
-
-function criarBotaoConcluir() {
-    let novoItem = lista.lastChild; //Novamente
-    let divPendentes = document.querySelector(".pendentes"); //divPendentes = div class="pendentes"
-    let ul = document.getElementById("lista"); //ul = #lista
-    let lastChild = ul.lastChild; //lastChild = último filho da lista
-    if (novoItem) { //Se existir um novoItem
-        let botao = document.createElement("input"); //Criou um elemento input
-        botao.type = "button"; //Type button
-        botao.id = "botao-concluir" //id "botao-concluir"
-        botao.value = 'Concluir "' + lastChild.innerHTML + '"'; //value = 'concluir' + nome da ultima tarefa
-        botao.onclick = function () { // onclick ele executa essas duas funções
-            excluirBotaoConcluir();
-            transferirTarefa()
-        }
-        divPendentes.appendChild(botao); //botao agora é um filho da div.pendentes
-    }
-}
-
-function excluirBotaoConcluir() {
-    let botao = document.getElementById("botao-concluir");
-    lista.appendChild(botao);
-    lista.removeChild(botao); //removeu
-}
-
-function limparConcluidas() {
-
-}
+    
+})
